f483d482146b5ed21f9dcd3659bdf67bd80667ba
Filipe Marques <filipi.marques@gmail.com>
2022-12-17 18:34:37 -0300

    simple example with core data

---
+++ countCalories/Views/AddFoodView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+struct AddFoodView: View {
+    
+    @Environment(\.managedObjectContext) var managedObject
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var calories = 0.0
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Food name", text: $name)
+                
+                VStack {
+                    Text("Calories: \(Int(calories))")
+                    Slider(value: $calories, in: 0...2000, step: 10)
+                }
+                .padding()
+                
+                HStack {
+                    Spacer()
+                    Button("Submit") {
+                        DataController().addFood(name: name, calories: calories, context: managedObject)
+                        dismiss()
+                    }
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+struct AddFoodView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFoodView()
+    }
+}
