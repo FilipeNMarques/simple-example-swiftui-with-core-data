6e6768a9b3300aa6f81a3dbaec6edc145f456cc3
Filipe Marques <filipi.marques@gmail.com>
2022-12-17 16:34:51 -0300

    Initial Commit

---
+++ countCalories/countCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  countCaloriesApp.swift
+//  countCalories
+//
+//  Created by Filipe Marques on 17/12/22.
+//
+
+import SwiftUI
+
+@main
+struct countCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
